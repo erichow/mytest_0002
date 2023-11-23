330a762944df526f2ae8d33a030438920e071cd4
zhoudm5 <zhoudm5@sany.com.cn>
2023-11-23 13:45:20 +0800

    modify main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -6,3 +6,5 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// const x = 0
